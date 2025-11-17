ea4f15b6a85766c112b51da7b4aba1868b15b070
GraceC-326 <grace.a.cochran@maine.edu>
2025-11-17 10:15:48 -0500

    Added hash_table.py and MOCK_DATA.csv
    Commit One(1) of my hash table homework. Uses a linear probing insert method with the size of the tables being the same as the size of the csv dataset. There were no empty spaces left between the two hash tables, however, the time taken for both tables to be created was about 15 seconds. In addition, the amount of collisions between the two hash tables was quite large, with 91550138 collisions from the title-oriented hash table, and 96570520 collisions from the  quote-oriented hash table. I suspect the hash-key calculating function is what's causing this amount of collisions, and I plan on improving it in my next commit. I also hope that this commit will be my least optimized version of these hash tables, so that I may see heavy improvements when i get to my fifth and final commit.

---
+++ hash_table.py
@@ -0,0 +1,102 @@
+import csv
+import time
+
+# Grace Cochran
+# COS 226 Hw 5
+# Creating, Analyzing, and Improving Hash Tables 
+
+class DataItem:	# Takes a given movie's statistics and stores it in its own list to be used.
+    def __init__(self, line):
+        self.movie_name = line[0]
+        self.genre = line[1]
+        self.release_date = line[2]
+        self.director = line[3]
+        self.box_office_revenue = float(line[4][1:])
+        self.rating = float(line[5])
+        self.duration_minutes = int(line[6])
+        self.production_company = line[7]
+        self.quote = line[8]
+
+class Node: # To be used for the linked list insertion method
+	def __init__(self, data):
+		self.data = data
+		self.next = None
+
+def hashFunct(stringData):
+    # does some mathematics to make a hash key, supposedly with the use of prime numbers
+    key = (len(stringData) * ord(stringData[0])) + 761
+    return key
+
+def linearInsert(table, index, data): # inserts data into a hash table through linear probing, returning the amount of collisions and empty spots filled once a place has been found for the data to be stored in the table. 
+	placeFound = False
+	collisions = 0
+	emptySpots = 0
+
+	while placeFound == False:
+		if table[index] != None:
+			collisions += 1
+			if index + 1 >= len(table):
+				index = 0
+			else:
+				index += 1
+		else:
+			table[index] = data
+			placeFound = True
+			emptySpots += 1
+	return collisions, emptySpots
+
+def linkInsert(table, index, node): # inserts data into a hash table through linked list insertion, adding to an index's linked list when a collision occurs, returning the amount of collisions and empty spots afterwards. 
+	pass
+
+
+def main():
+
+	file = "MOCK_DATA.csv"
+	counter = 0
+
+	size = 15000 # initializing the hash tables and setting parameters 
+	hashTitleTable = [None] * size
+	hashQuoteTable = [None] * size
+	titleColTotal = 0
+	titleWasteTotal = size
+	quoteColTotal = 0
+	quoteWasteTotal = size
+	totalWaste = 0
+
+	start = time.time()
+	with open(file, "r", newline = "", encoding = "utf8") as csvfile:
+		data = csv.reader(csvfile) # reading and making a dataItem for each read row
+		for row in data:
+			# print(row)
+			if counter == 0:
+				counter += 1
+				continue
+			movie = DataItem(row)
+
+			titleKey = hashFunct(movie.movie_name) # getting the keys of the appropriate fields
+			quoteKey = hashFunct(movie.quote)
+			titleIndex = titleKey % len(hashTitleTable) # getting the indexes of the row's data
+			quoteIndex = quoteKey % len(hashQuoteTable)
+
+			# inserting the DataItems into the hash tables
+			titleCollisions, titleFilled = linearInsert(hashTitleTable, titleIndex, movie) 
+			quoteCollisions, quoteFilled = linearInsert(hashQuoteTable, quoteIndex, movie)
+			# linkInsert(hashTitleTable, titleIndex, Node(movie)) 
+			# linkInsert(hashQuoteTable, quoteIndex, Node(movie))
+	
+			titleColTotal += titleCollisions # updating the total collisions and empty indexes
+			quoteColTotal += quoteCollisions
+			titleWasteTotal -= titleFilled
+			quoteWasteTotal -= quoteFilled
+			counter += 1
+	
+	end = time.time()
+	print("Commit One(1). Initial creation with linear probing insert method.\n")
+	print(f"Hash table creation sucessful, went through loop {counter} times.")
+	print(f"Construction of hash tables took {end-start:0.2f} seconds.")
+	print(f"The title-oriented hash table encountered {titleColTotal} collisions, while the quote-oriented hash table encountered {quoteColTotal} collisions.")
+	print(f"The amount of wasted space from the title-oriented hash table was {titleWasteTotal} indexes, while the quote-oriented hash table wasted {quoteWasteTotal} indexes.")
+
+
+if __name__ == "__main__":
+    main()
